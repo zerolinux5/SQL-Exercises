cf1d8749128308e7b625ed991f24282f13b0f81b
Jesus Magana <maganazero5@gmail.com>
2014-08-04 21:21:48 -0700

    ex11 replacing data

---
+++ ex11.sql
@@ -0,0 +1,12 @@
+INSERT INTO person (id, first_name, last_name, age)
+	VALUES (0, "Frank", "Smith", 100);
+
+INTERT OR REPLACE INTO person (id, first_name, last_name, age)
+	VALUES (0, "Frank", "Smith", 100);
+
+SELECT * FROM person;
+
+REPLACE INTO person (id, first_name, last_name, age)
+	VALUES (0, "Zero", "Linux", 21);
+
+SELECT * FROM person;
